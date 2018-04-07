d43c31ca64fbe9391fbdcfb6122a7d4210794f31
thepaperpilot <thepaperpilot@gmail.com>
2018-04-06 22:39:23 -0500

    Initial Commit

---
+++ Assets/Scripts/Drug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drug : MonoBehaviour {
+
+    public class DrugState {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Slug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Slug : MonoBehaviour {
+    
+    public Image body;
+    public float minSize = .8f;
+    public float maxSize = 1.2f;
+
+    private List<Drug.DrugState> drugs = new List<Drug.DrugState>();
+
+    public void Generate() {
+        transform.localScale = Vector3.one * Random.Range(minSize, maxSize);
+        body.color = Random.ColorHSV(0, 1, .5f, 1, 1, 1);
+    }    
+}
